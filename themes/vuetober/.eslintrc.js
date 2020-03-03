1fcd949704ecadbdf86ab61e39ee94134063d3b2
jakubkoje <jakubkoje@gmail.com>
2020-03-03 19:44:51 +0100

    added vuetober

--- themes/vuetober/.eslintrc.js
+++ themes/vuetober/.eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+
+  extends: ['plugin:vue/essential', 'plugin:vue/strongly-recommended', '@vue/airbnb'],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
 };
