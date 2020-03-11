6d5a59e8e048330a5413a978bdb59ee087886808
adamtibombu <adam@kalmar-sk.eu>
2020-03-11 21:20:42 +0100

    vytvoril som plugin teachers

---
+++ plugins/teachplusplus/teachers/models/Teacher.php
@@ -0,0 +1,29 @@
+<?php namespace Teachplusplus\Teachers\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Teacher extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+    
+    /*
+     * Disable timestamps by default.
+     * Remove this line if timestamps are defined in the database table.
+     */
+    public $timestamps = false;
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'teachplusplus_teachers_';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+}
