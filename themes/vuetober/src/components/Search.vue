2bf57b886195eb8e03c1541c3d49eea849ccee42
jakubkoje <jakubkoje@gmail.com>
2020-03-23 12:21:33 +0100

    small navbar fix

--- themes/vuetober/src/components/Search.vue
+++ themes/vuetober/src/components/Search.vue
@@ -1,33 +1,42 @@
 <template>
   <div>
-    <b-form-input
-      class="my-3"
-      type="text"
-      name="Učiteľ"
-      placeholder="Meno učiteľa"
-      v-model="searchTeacher"
-    />
-    <b-list-group>
-      <b-list-group-item
-        v-for="teacher in teachers"
-        :key="teacher.id"
-        class="d-flex align-items-center"
-      >
-        <h5 class="mr-auto my-auto">
-          {{ teacher.name }} {{ teacher.surname }}
-        </h5>
-        <h6
-          class="my-auto ml-2"
-          v-for="subject in teacher.subjects"
-          :key="subject.id"
+    <b-row>
+      <b-col class="d-flex align-items-center">
+        <h6 class="my-auto">
+          Teach++
+        </h6>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-form-input
+        class="my-3"
+        type="text"
+        name="Učiteľ"
+        placeholder="Meno učiteľa"
+        v-model="searchTeacher"
+      />
+      <b-list-group>
+        <b-list-group-item
+          v-for="teacher in teachers"
+          :key="teacher.id"
+          class="d-flex align-items-center"
         >
-          {{ subject.short }}
-        </h6>
-        <h6 class="my-auto ml-2">
+          <h5 class="mr-auto my-auto">
+            {{ teacher.name }} {{ teacher.surname }}
+          </h5>
+          <h6
+            class="my-auto ml-2"
+            v-for="subject in teacher.subjects"
+            :key="subject.id"
           >
-        </h6>
-      </b-list-group-item>
-    </b-list-group>
+            {{ subject.short }}
+          </h6>
+          <h6 class="my-auto ml-2">
+            >
+          </h6>
+        </b-list-group-item>
+      </b-list-group>
+    </b-row>
   </div>
 </template>
 
