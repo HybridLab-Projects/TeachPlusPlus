6bd45e5a493717f54e19f746b0dbe41a8643837d
adamtibombu <adam@kalmar-sk.eu>
2020-03-29 22:37:27 +0200

    feedback belongs to relationships on teacher in routes

--- plugins/teachplusplus/teachers/routes.php
+++ plugins/teachplusplus/teachers/routes.php
@@ -1,6 +1,7 @@
 <?php
 use Teachplusplus\Teachers\Models\Teacher;
 use Teachplusplus\Teachers\Models\Feedback;
+use Teachplusplus\Teachers\Models\Subject;
 
 Route::group(['prefix' => 'api'], function () {
 Route::get('teacher', function () {
@@ -22,11 +23,16 @@ Route::get('teacher/{id}', function ($id) {
 
 Route::post('feedback', function () {
 
-
     $data = request()->only([
         'content'
     ]);
-    Feedback::create($data);
-    });
+    $feedback = Feedback::create($data);
+    
+    $teacher = Teacher::find(2);
 
+    $feedback->teacher()->associate($teacher);
+
+    $feedback->save();
+
+    });
 });
