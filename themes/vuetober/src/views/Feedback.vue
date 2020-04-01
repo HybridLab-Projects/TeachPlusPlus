4fb157145163e9ab715f0641ba8c7a0ba00eda04
jakubkoje <jakubkoje@gmail.com>
2020-04-01 15:15:32 +0200

    code fixes and grammar

--- themes/vuetober/src/views/Feedback.vue
+++ themes/vuetober/src/views/Feedback.vue
@@ -10,48 +10,51 @@
         <div class="pl-3">
           <div class="pt-5">
             <h6 class="my-5 font-weight-bold">
-              Napoveda
+              Nápoveda
             </h6>
           </div>
           <p class="mb-5 pb-3">
-            Ak si sa rozhodol napisat feedback na konkretneho ucitela,
-            v klude sa mozes inspirovat touto sablonou.
+            Ak si sa rozhodol napísať feedback na konkrétneho učiteľa,
+            v kľude sa môžeš inšpirovať touto šablónou.
           </p>
         </div>
         <hr class="border-white">
         <div class="pl-3">
           <p class="mt-5 pt-5">
-            Mily pan ucitel,<br><br>
+            Milý pán učiteľ,<br><br>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Magni temporibus provident mollitia exercitationem in
             voluptatum ut assumenda iste consequuntur cum.
           </p>
         </div>
       </b-col>
-      <b-col cols="9">
+      <b-col
+        cols="9"
+        class="mt-4"
+      >
         <div class="d-flex justify-content-end mt-3">
-          <p class="text-secondary my-auto mr-3">
-            <strong class="">
-              Nechcem dat feedback
-            </strong>
+          <p class="text-muted my-auto mr-2 font-weight-bold">
+            Nechcem dať feedback
           </p>
           <img
             :src="require(`@/assets/img/Cross.png`)"
-            alt=""
+            alt="x"
           >
         </div>
-        <b-col class="d-flex align-items-center">
+        <div class="d-flex align-items-center">
           <img
             :src="require(`@/assets/img/feedplus.png`)"
-            alt=""
+            alt="+"
           >
-          <div class="ml-5 mb-3 col-md-9">
-            <h5 class="text-secondary">
-              <strong>SPSE Halova</strong>
-            </h5>
-            <h1><strong>Pridat feedback</strong></h1>
-            <p class="text-secondary">
-              Pridajte si ucitela a predmet, ktory vyucuje
+          <div class="ml-5 mb-3 w-100">
+            <h6 class="text-muted font-weight-bold">
+              SPŠE Hálova
+            </h6>
+            <h1 class="font-weight-bold">
+              Pridať feedback
+            </h1>
+            <p class="text-muted">
+              Pridajte si učiteľa a predmet, ktorý vyučuje
             </p>
             <b-form
               inline
@@ -72,23 +75,22 @@
                 class="px-3 py-2"
                 @click.prevent="createFeedback"
               >
-                Odoslat feedback
+                Odoslať feedback
               </b-button>
             </b-form>
           </div>
-        </b-col>
+        </div>
         <hr>
 
-          <b-form>
-            <b-form-textarea
-              id="textarea-rows"
-              placeholder="Tall textarea"
-              rows="8"
-              class="textfield"
-              v-model="feedback"
-            />
-          </b-form>
-        </div>
+        <b-form>
+          <b-form-textarea
+            id="textarea-rows"
+            placeholder="Tall textarea"
+            rows="8"
+            class="textfield"
+            v-model="feedback"
+          />
+        </b-form>
       </b-col>
     </b-row>
   </b-container>
@@ -122,10 +124,9 @@ $purpleColor: #5352f6;
 
 .form1 {
   width: 30%;
-  background-color: #f8f5f3;
+  background-color: #F4F5F8;
   border-radius: 0px;
   border: none;
-  border-bottom: 2px solid $purpleColor;
 }
 
 .textfield {
