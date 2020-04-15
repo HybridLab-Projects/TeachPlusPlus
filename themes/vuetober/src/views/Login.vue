de1c87c5a7783a005aea9ce085405b319b1d796a
jakubkoje <jakubkoje@gmail.com>
2020-04-15 18:18:43 +0200

    fixed layout on login and register

--- themes/vuetober/src/views/Login.vue
+++ themes/vuetober/src/views/Login.vue
@@ -1,59 +1,64 @@
 <template>
-  <div class="row">
-    <div class="col-md-6 my-auto p-0">
-      <h1 class="d-flex justify-content-center test2 mb-5">
-        LOGIN
-      </h1>
-      <div class="d-flex justify-content-center">
-        <ValidationObserver v-slot="{ passes }">
-          <b-form @submit.enter.prevent="passes(onLogin)">
-            <BTextInputWithValidation
-              rules="required"
-              type="text"
-              label=""
-              name="Používateľské meno"
-              v-model="user.username"
-              placeholder="Zadaj používateľské meno"
-              class=""
-            />
-
-            <BTextInputWithValidation
-              rules="required|min:8|upperCase|containsNumber"
-              type="password"
-              label=""
-              name="Heslo"
-              vid="user.password"
-              v-model="user.password"
-              placeholder="Zadaj heslo"
-            />
-
-            <b-button
-              class="shadow font-weight-bold py-10 px-5 mt-5"
-              type="submit"
-              variant="danger"
-            >
-              Submit
-            </b-button>
-            <b-alert
-              show
-              variant="danger"
-              v-if="error"
-            >
-              {{ error }}
-            </b-alert>
-          </b-form>
-        </ValidationObserver>
-      </div>
-    </div>
-    <div class="col-md-2 p-0" />
-    <div class="d-flex justify-content-center col-md-4 p-0 mb-5 cont_img">
-      <img
-        class="cont_img_pic"
-        :src="require(`@/assets/img/Group.svg`)"
-        alt
+  <b-container fluid>
+    <b-row>
+      <b-col
+        class="my-auto"
+        cols="6"
       >
-    </div>
-  </div>
+        <h1 class="d-flex justify-content-center test2 mb-5">
+          Prihlásenie
+        </h1>
+        <div class="d-flex justify-content-center">
+          <ValidationObserver v-slot="{ passes }">
+            <b-form @submit.enter.prevent="passes(onLogin)">
+              <BTextInputWithValidation
+                rules="required"
+                type="text"
+                label=""
+                name="Používateľské meno"
+                v-model="user.username"
+                placeholder="Zadaj používateľské meno"
+                class=""
+              />
+
+              <BTextInputWithValidation
+                rules="required|min:8|upperCase|containsNumber"
+                type="password"
+                label=""
+                name="Heslo"
+                vid="user.password"
+                v-model="user.password"
+                placeholder="Zadaj heslo"
+              />
+
+              <b-button
+                class="shadow font-weight-bold py-10 px-5 mt-5"
+                type="submit"
+                variant="danger"
+              >
+                Submit
+              </b-button>
+              <b-alert
+                show
+                variant="danger"
+                v-if="error"
+              >
+                {{ error }}
+              </b-alert>
+            </b-form>
+          </ValidationObserver>
+        </div>
+      </b-col>
+      <div class="col-md-2 p-0" />
+      <div class="d-flex justify-content-center col-md-4 p-0 mb-5 cont_img">
+        <img
+          class="cont_img_pic"
+          :src="require(`@/assets/img/Group.svg`)"
+          alt
+        >
+      </div>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
