cd12f1734b8e77da52c4728275f0c6f9084f42d1
MarkMucska <mark.mucska.work@gmail.com>
2020-04-26 21:15:05 +0200

    uvodna stranka resp

--- themes/vuetober/src/views/Home.vue
+++ themes/vuetober/src/views/Home.vue
@@ -6,13 +6,17 @@
     <b-row class="cont-color">
       <b-col
         cols="5"
-        class="pl-5"
+        class="pl-5 justify-container-center"
       >
-        <div class="content-container pl-5">
-          <h2 class="display-4 mb-4 text-white">
+        <div
+          class="content-container pl-5"
+        >
+          <h2
+            class="display-4 header mb-4 text-white"
+          >
             Čo je to, <strong>Teach++</strong>
           </h2>
-          <p class="text-white">
+          <p class="text-white popis">
             <strong>
               Zanechaj učiteľovi spätnú väzbu, vďaka ktorej bude hodina pre obe strany príjemnejšia!
             </strong>
@@ -56,16 +60,17 @@ export default {
 
 <style lang="scss" scoped>
 .cont-color {
-  height: 80vh;
-  width: 97vw;
+  height: 80vh auto;
+  width: 95vw;
   background: linear-gradient(45deg, #5352f6 0%, #8381fd 100%);
-  border-radius: 0px 0px 0px 10rem;
+  border-radius: 0px 0px 100px 10rem;
   position: absolute;
   z-index: -10;
 }
 
 .content-container {
-  padding-top: 15rem;
+  width: auto;
+  padding-top: 10rem;
   padding-left: 0px;
   max-width: 550px;
   min-width: 430px;
@@ -73,8 +78,37 @@ export default {
   z-index: 100;
 }
 
+@media screen and (max-width: 1000px) {
+  .content-container {
+    padding-top: 5rem;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .content-container {
+    .header {
+      font-size: 3rem;
+    }
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .content-container {
+    .header {
+      font-size: 2rem;
+    }
+
+    .popis {
+      width: 75%;
+      font-size: 1rem;
+      margin: 0;
+    }
+  }
+}
+
 .cont-img-pic {
-  height: 80vh;
+  height: auto;
+  width: 100%;
   transform: translateX(0%) translateY(20%);
 }
 
@@ -85,5 +119,9 @@ export default {
 
 .footer-text {
   font-size: 0.875rem;
+}
+
+.alll {
+  overflow-x: none;
 }
 </style>
