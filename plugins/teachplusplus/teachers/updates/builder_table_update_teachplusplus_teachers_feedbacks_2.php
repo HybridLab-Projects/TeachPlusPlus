e96eb99a68023700013b94e24a5fb16d1f2ed60e
jakubkoje <jakubkoje@gmail.com>
2020-04-26 21:16:24 +0200

    fixed table

--- plugins/teachplusplus/teachers/updates/builder_table_update_teachplusplus_teachers_feedbacks_2.php
+++ plugins/teachplusplus/teachers/updates/builder_table_update_teachplusplus_teachers_feedbacks_2.php
@@ -7,16 +7,14 @@ class BuilderTableUpdateTeachplusplusTeachersFeedbacks2 extends Migration
 {
     public function up()
     {
-        Schema::table('teachplusplus_teachers_feedbacks', function($table)
-        {
-            $table->boolean('banned')->nullable(false)->default(0)->change();
+        Schema::table('teachplusplus_teachers_feedbacks', function ($table) {
+            $table->boolean('banned')->nullable(false)->default(false)->change();
         });
     }
     
     public function down()
     {
-        Schema::table('teachplusplus_teachers_feedbacks', function($table)
-        {
+        Schema::table('teachplusplus_teachers_feedbacks', function ($table) {
             $table->boolean('banned')->nullable()->default(null)->change();
         });
     }
