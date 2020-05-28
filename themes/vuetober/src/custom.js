50e3f497a83d6b3598280924b6d99cc4fbb00abd
jakubkoje <jakubkoje@gmail.com>
2020-05-28 13:38:54 +0200

    vue-gtag initial setup

--- themes/vuetober/src/custom.js
+++ themes/vuetober/src/custom.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import Axios from 'axios';
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import './custom.scss';
 
@@ -7,48 +9,64 @@ import { ValidationProvider, extend } from 'vee-validate';
 import * as rules from 'vee-validate/dist/rules';
 import { messages } from 'vee-validate/dist/locale/sk.json';
 
-import Axios from 'axios';
+import VueGtag from 'vue-gtag';
 
-export function setup() {
-  Vue.use(BootstrapVue);
-  Vue.use(IconsPlugin);
+import router from './router';
+import store from './store';
 
-  Object.keys(rules).forEach((rule) => {
-    extend(rule, {
-      ...rules[rule], // copies rule configuration
-      message: messages[rule], // assign message
+
+const custom = {
+  setup() {
+    Vue.use(BootstrapVue);
+    Vue.use(IconsPlugin);
+
+    Vue.use(VueGtag, {
+      config: {
+        id: 'UA-167852978-1',
+      },
+      router,
     });
-  });
 
-  extend('upperCase', {
-    validate: (value) => value !== value.toLowerCase(),
-    message: '{_field_} musí obsahovať veľké písmeno',
-  });
-  extend('containsNumber', {
-    validate: (value) => /\d/.test(value),
-    message: '{_field_} musí obsahovať číslicu',
-  });
+    Object.keys(rules).forEach((rule) => {
+      extend(rule, {
+        ...rules[rule], // copies rule configuration
+        message: messages[rule], // assign message
+      });
+    });
 
-  Vue.component('ValidationProvider', ValidationProvider);
-}
+    extend('upperCase', {
+      validate: (value) => value !== value.toLowerCase(),
+      message: '{_field_} musí obsahovať veľké písmeno',
+    });
+    extend('containsNumber', {
+      validate: (value) => /\d/.test(value),
+      message: '{_field_} musí obsahovať číslicu',
+    });
 
-export function created(router, store) {
-  const token = localStorage.getItem('token');
-  if (token) {
-    // eslint-disable-next-line dot-notation
-    Axios.defaults.headers.common['Authorization'] = `Bearer ${
-      token
-    }`;
-  }
+    Vue.component('ValidationProvider', ValidationProvider);
+  },
 
-  Axios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if ((error.response.status === 401 && error.response.data.error === 'token_expired')
-      || (error.response.status === 400 && error.response.data.error === 'token_invalid')) {
-        store.dispatch('logout').finally(() => router.push('/'));
-      }
-      return Promise.reject(error);
-    },
-  );
-}
+  created() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // eslint-disable-next-line dot-notation
+      Axios.defaults.headers.common['Authorization'] = `Bearer ${
+        token
+      }`;
+    }
+
+    Axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if ((error.response.status === 401 && error.response.data.error === 'token_expired')
+        || (error.response.status === 400 && error.response.data.error === 'token_invalid')) {
+          store.dispatch('logout').finally(() => router.push('/'));
+        }
+        return Promise.reject(error);
+      },
+    );
+  },
+
+};
+
+export default custom;
